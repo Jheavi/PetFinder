632cdc7f4527e5bf007f7019cf7769496907a387
Jheavi <javierlaso213@hotmail.com>
2021-03-05 16:15:04 +0100

    changed sensitive variables to env variables

--- src/actions/firebase/firebaseConfig.js
+++ src/actions/firebase/firebaseConfig.js
@@ -1,15 +1,12 @@
-import sensitiveVariables from '../../sensitiveVariables';
 
 const firebaseConfig = {
-	apiKey: sensitiveVariables.apiKey || process.env.apiKey,
-	authDomain: 'petfinder-skylab.firebaseapp.com' || process.env.authDomain,
-	databaseURL:
-		'https://petfinder-skylab.firebaseio.com' || process.env.databaseURL,
-	projectId: 'petfinder-skylab' || process.env.projectId,
-	storageBucket: 'petfinder-skylab.appspot.com' || process.env.storageBucket,
-	messagingSenderId:
-		sensitiveVariables.messagingSenderId || process.env.messagingSenderId,
-	appId: sensitiveVariables.appId || process.env.appId
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_DATABASE_URL,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_IP,
+	appId: process.env.REACT_APP_APP_ID
 };
 
 export default firebaseConfig;
